94fefff6245e6e92cef0f663411f9e08745c6e7a
David P <david@paluy.org>
2012-01-27 16:43:13 +0200

    fix validation

--- app/models/vol_value.rb
+++ app/models/vol_value.rb
@@ -1,21 +1,23 @@
 class VolValue
   include Mongoid::Document
   include Mongoid::MultiParameterAttributes
-  
+
   field :value, :type => Integer
   field :timestamp, :type => Date
-  
+
   embedded_in :volatility
-  
+
   validates_presence_of :value, :timestamp
   validates_uniqueness_of :timestamp
-  
+validates_numericality_of :value, :presence => true
+
   before_create :update_value
-  
+
   private
-  
+
   def update_value
-    self.value *= 100  
+    self.value *= 100
   end
-  
+
 end
+
